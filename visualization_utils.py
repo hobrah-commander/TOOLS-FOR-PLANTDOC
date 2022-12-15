b897a49b30b6611f19cd31f3d92579855c9d1922
hobrah-commander <120438161+hobrah-commander@users.noreply.github.com>
2022-12-14 23:08:05 -0700

    Update visualization_utils.py

--- visualization_utils.py
+++ visualization_utils.py
@@ -1,1 +1,26 @@
+# visualizations_utils.py
+
+import matplotlib.pyplot as plt
+
+def visualize_data_loading_and_preprocessing(train_file, val_file, test_file):
+    # Load and preprocess the data
+    X_train, y_train, X_val, y_val, X_test, y_test = load_data(train_file, val_file, test_file)
+    X_train, y_train = preprocess_data(X_train, y_train)
+    X_val, y_val = preprocess_data(X_val, y_val)
+    X_test, y_test = preprocess_data(X_test, y_test)
+
+    # Create a visual representation of the data loading and preprocessing steps
+    # For example:
+    plt.plot(X_train)
+    plt.show()
+
+def visualize_model_training_and_evaluation(model, X_train, y_train, X_val, y_val):
+    # Train and evaluate the model
+    history = model.fit(X_train, y_train, validation_data=(X_val, y_val))
+
+    # Create a visual representation of the model training and evaluation process
+    # For example:
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.show()
 
